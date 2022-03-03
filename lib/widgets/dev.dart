015cd6f7a143e3077ae65ff1bbb139809867d6d4
abkraynak <andrewbkraynak@gmail.com>
2022-03-03 16:48:26 -0500

    Cleanup

--- lib/widgets/dev.dart
+++ lib/widgets/dev.dart
@@ -4,6 +4,47 @@ import '../constants/buttons.dart';
 import '../constants/positioning.dart';
 import '../pages/home.dart';
 
+import 'package:flutter_blue/flutter_blue.dart';
+
+
+/*
+BluetoothDevice device;
+BluetoothState state;
+BluetoothDeviceState deviceState;
+
+///Initialisation and listening to device state@override
+@override
+void initState() {
+  super.initState();
+
+  //checks bluetooth current state
+  FlutterBlue.instance.state.listen((state) {
+    if (state == BluetoothState.off) {
+      //Alert user to turn on bluetooth.
+    } else if (state == BluetoothState.on) {
+      //if bluetooth is enabled then go ahead.
+      // Make sure user's device gps is on.
+      scanForDevices();
+    }
+  });
+}
+
+void scanForDevices() async {
+  scanSubscription = bluetoothInstance.scan().listen((scanResult) async {
+    if (scanResult.device.name == "your_device_name") {
+      print("found device");
+      device = scanResult.device; //Assigning bluetooth device
+      stopScanning(); //After that we stop the scanning for device
+}});}
+
+void stopScanning() {
+  bluetoothInstance.stopScan();
+  scanSubscription.cancel();
+}
+
+*/
+
+
 class Dev extends StatelessWidget {
   @override
   Widget build(BuildContext context) {

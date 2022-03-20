fc1553d0dfd78730f694a84496fc11828c171a7e
abkraynak <andrewbkraynak@gmail.com>
2022-03-20 18:17:28 -0400

    Added device distance display widget

--- lib/widgets/dev.dart
+++ lib/widgets/dev.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../constants/buttons.dart';
+import '../functions/bluetooth_scanning.dart';
+import '../models/device.dart';
 import '../constants/positioning.dart';
-// import '../pages/home.dart';
 
 class Dev extends StatelessWidget {
   @override
@@ -10,25 +10,51 @@ class Dev extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: <Widget>[
-          Card(
-              child: ListTile(
-                  title: const Text(''),
-                  subtitle: const Text(''),
-                  onTap: () {})),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: Paddings.ver, horizontal: Paddings.hor),
-            child: ElevatedButton(
-              child: Text('Connect', style: ElevatedButtons.mainTextStyle),
-              style: ElevatedButtons.mainButtonStyle,
-              onPressed: () {
-                /* Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => HomePage())); */
-              },
-            ),
-          ),
+          DeviceDistance(),
         ],
       ),
     );
   }
+}
+
+class DeviceDistance extends StatelessWidget {
+  final List<Device> deviceList;
+  DeviceDistance({this.deviceList});
+
+  @override
+  Widget build(BuildContext context) {
+    final double minDistance = getMinDistance(deviceList);
+    final double signalStrength = convertDistanceToScale(minDistance, 1.83, 6);
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          vertical: Paddings.ver, horizontal: Paddings.hor),
+      child: deviceList != null && deviceList.isNotEmpty
+          ? distanceText(minDistance)
+          : Text(
+              'No nearby devices',
+              textScaleFactor: 1.2,
+            ),
+    );
+  }
+}
+
+double getMinDistance(List<Device> deviceList) {
+  var minDistance = 999.9;
+  if (deviceList != null) {
+    deviceList.forEach((device) {
+      if (device.RSSIAverage != null) {
+        var deviceDistance = convertSignalToDistance(device.RSSIAverage);
+        if (deviceDistance < minDistance) {
+          minDistance = deviceDistance;
+        }
+      }
+    });
+  }
+  return minDistance;
+}
+
+Text distanceText(double distance) {
+  return Text("Distance: ${metersToFeet(distance).toStringAsFixed(2)} ft",
+      textScaleFactor: 1.2);
 }
